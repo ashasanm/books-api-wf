5905f5141d1cb4c05a88fc7b5567d9a76a605068
Hasan <anas.hasan@loratechai.com>
2026-03-27 01:09:10 +0700

    test: update conftest default region to sea

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ import os
 # This ensures moto and repository.py both see the correct values
 # regardless of test execution order.
 os.environ["DYNAMODB_TABLE"] = "books-integration"
-os.environ["AWS_DEFAULT_REGION"] = "us-east-1"
+os.environ["AWS_DEFAULT_REGION"] = "ap-southeast-2"
 os.environ["AWS_ACCESS_KEY_ID"] = "testing"
 os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
 os.environ["AWS_SECURITY_TOKEN"] = "testing"
