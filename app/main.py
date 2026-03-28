5c35a12f25d8b81a3ad90e0a2cacc55a3189b3c7
ashasanm <anassufi007@gmail.com>
2026-03-28 08:56:48 +0700

    chore: add CORS middleware for handling cross-origin requests

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from mangum import Mangum
 from app.routers import books
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="Books API",
@@ -9,6 +10,12 @@ app = FastAPI(
 )
 
 app.include_router(books.router, prefix="/api")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["GET", "POST", "OPTIONS"],
+    allow_headers=["*"],
+)  # this should be changed to predefined web app url, and allowed headers
 
 # Lambda handler (Mangum adapts ASGI → API Gateway)
 handler = Mangum(app, lifespan="off")
