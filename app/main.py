cf218cad91a5c003bdec6ad6161414d4af8d041f
ashasanm <anassufi007@gmail.com>
2026-04-03 06:00:13 +0700

    refactor:add api versioning v1

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from mangum import Mangum
 from app.routers import books
-from fastapi.middleware.cors import CORSMiddleware
-from app.settings import settings
+
+API_V1_PREFIX = "/api/v1"
 
 app = FastAPI(
     title="Books API",
@@ -10,13 +11,15 @@ app = FastAPI(
     version="1.0.0",
 )
 
-app.include_router(books.router, prefix="/api")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.allowed_origins,
-    allow_methods=["GET", "POST", "OPTIONS"],
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
     allow_headers=["*"],
-)  #TODO: this should be changed to predefined web app url, and allowed headers
+)
 
-# Lambda handler (Mangum adapts ASGI → API Gateway)
+app.include_router(books.router, prefix=API_V1_PREFIX)
+
+# Lambda handler (Mangum adapts ASGI -> API Gateway)
 handler = Mangum(app, lifespan="off")
